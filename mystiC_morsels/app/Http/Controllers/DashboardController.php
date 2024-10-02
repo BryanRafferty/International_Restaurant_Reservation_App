e32193d1da5dff6de9c9b31ebf6806f504629bab
miawpuspita <puspitasari22232@gmail.com>
2024-10-02 10:51:31 +0700

    push

--- mystiC_morsels/app/Http/Controllers/DashboardController.php
+++ mystiC_morsels/app/Http/Controllers/DashboardController.php
@@ -8,9 +8,9 @@ use Illuminate\Support\Facades\DB;
 class DashboardController extends Controller
 {
     public function index(){
-        $pinjamanggota = DB::select("SELECT anggotas.nama_anggota, COUNT(*) as jumlah_pinjam FROM pinjams 
-        JOIN anggotas ON pinjams.anggota_id = anggotas.id GROUP BY anggotas.nama_anggota;");
-        return view('dashboard')->with('pinjamanggota',$pinjamanggota);
+        $pesananpelangan= DB::select("SELECT pelanggann.nama_pelangan, COUNT(*) as jumlah_pesanan FROM pesanann
+        JOIN pelangann ON pesanan.pelangan_id = pelangann.id GROUP BY pelangann.nama_pelangan;");
+        return view('dashboard')->with('pesananpelangan',$pesananpelangan);
     }
 
 
